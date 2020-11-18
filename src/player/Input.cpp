aca993b6ebdf071582c4507f1e1efabd2ba00199
vic <vpicon98@gmail.com>
2020-11-18 21:12:48 +0100

    Add empty destructor to Input abstract class

---
+++ src/player/Input.cpp
@@ -0,0 +1,25 @@
+/**
+ * Input.cpp
+ *
+ * Module: player
+ *
+ * Implements Input abstract class.
+ *
+ */
+
+#include "Input.h"
+
+
+namespace player {
+
+
+/**
+ * Empty destructor in the base class Input, for when the destructor
+ * is called from other derived classes' destructor.
+ */
+Input::~Input() { }
+
+
+
+}  // namespace player
+
