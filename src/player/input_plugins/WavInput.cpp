eb6d4e16ba979d6ebeda135dd3f05ce464a20dbb
vic <vpicon98@gmail.com>
2020-11-20 13:45:39 +0100

    Implement WavInput constructor and some helpers

--- src/player/input_plugins/WavInput.cpp
+++ src/player/input_plugins/WavInput.cpp
@@ -11,9 +11,28 @@
 #include "Buffer.h"
 
 #include <string>
+#include <fstream>
+#include <iostream>
 
 
 namespace player {
+
+
+
+WavInput::WavInput(const std::string& filename)
+    : _filename{filename},
+      _file{filename, std::ios_base::binary | std::ios_base::in}
+{
+    if (!_file) {
+        // TODO: define specific error types
+        throw std::runtime_error("Could not open file: " + filename);  
+    }
+
+    if ( !(readRIFFChunk() && readFormatChunk() && readDataChunkHeader()) ) {
+        // TODO: define specific error types
+        throw std::runtime_error("Could not read WAV format");
+    }
+}
 
 
 
@@ -22,20 +41,21 @@ size_t WavInput::read(Buffer::Position writePos) {
     return writePos.size();
 }
 
+
+
 // TODO: is a stub.
 void WavInput::seek(double seconds) {
     seconds = 0;
 }
+
+
 
 // TODO: is a stub.
 bool WavInput::reachedEOF() const {
     return false;
 }
 
-// TODO: is a stub.
-SampleFormat WavInput::getSampleFormat() const {
-    return {0, 0, 0, SampleFormat::Endian::little, SampleFormat::Encoding::signedEnc};
-}
+
 
 double WavInput::getDuration() const {
     return 0;
@@ -43,6 +63,58 @@ double WavInput::getDuration() const {
 
 
 
+bool WavInput::readRIFFChunk() {
+    char buf[4];
+
+    // Read RIFF
+    _file.read(buf, 4); 
+    if (!_file || std::string {buf} != "RIFF")
+        return false;
+
+    // Read int
+    uint32_t size;
+    _file.read(reinterpret_cast<char *>(&size), 4);
+    if (!_file)
+        return false;
+    size += 8;
+
+
+    // Read WAVE
+    _file.read(buf, 4); 
+    if (!_file || std::string {buf} != "WAVE")
+        return false;
+
+    // RIFF Header Chunk read successfully
+    return true;
+}
+
+
+
+// TODO: is a stub
+bool WavInput::readFormatChunk() {
+    return false;
+}
+
+
+
+bool WavInput::readDataChunkHeader() {
+    char buf[4];
+
+    // Read RIFF
+    _file.read(buf, 4); 
+    if (!_file || std::string {buf} != "data")
+        return false;
+
+    // Read int
+    uint32_t size;
+    _file.read(reinterpret_cast<char *>(&size), 4);
+    if (!_file)
+        return false;
+    _dataSize = size;
+
+    return true;
+}
+
+
+
 }  // namespace player
-
-
