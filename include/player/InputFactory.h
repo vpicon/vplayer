ec2a014102c03f841eb25564c2a60a74e73dd935
vic <vpicon98@gmail.com>
2020-11-23 09:27:12 +0100

    Add Input factory class interface and stub implementation

---
+++ include/player/InputFactory.h
@@ -0,0 +1,37 @@
+/**
+ * InputFactory.h
+ *
+ * Module: player
+ *
+ * Defines InputFactory class interface to create Input subclasses
+ * of the Input interface.
+ * Uses the SimpleFactory pattern.
+ *
+ */
+
+#ifndef _VPLAYER_INPUT_FACTORY_H
+#define _VPLAYER_INPUT_FACTORY_H
+
+
+#include <memory>
+#include <string>
+
+#include "Input.h"
+
+
+namespace player {
+
+
+class InputFactory {
+public:
+    static std::unique_ptr<Input> create(std::string filename, Buffer& buffer);
+
+    static std::string getExtension(std::string filename);
+};
+
+
+
+}  // namespace player
+
+
+#endif  // _VPLAYER_INPUT_FACTORY_H
---
+++ src/player/InputFactory.cpp
@@ -0,0 +1,39 @@
+/**
+ * InputFactory.cpp
+ *
+ * Module: player
+ *
+ * Implements the InputFactory class methods.
+ *
+ */
+
+#include "InputFactory.h"
+#include "input_plugins/WavInput.h"
+
+#include <string>
+
+namespace player {
+
+
+
+std::unique_ptr<Input> InputFactory::create(std::string filename, 
+                                            Buffer& buffer) 
+{
+    std::unique_ptr<Input> inputPtr;
+
+    std::string ext = InputFactory::getExtension(filename);
+    if (ext == "wav") 
+        inputPtr.reset(new WavInput{filename, buffer});
+
+    return inputPtr;
+}
+
+
+
+std::string InputFactory::getExtension(std::string filename) {
+    return filename;
+}
+
+
+
+}  // namespace player
