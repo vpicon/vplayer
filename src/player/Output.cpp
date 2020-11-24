c944b792391b5b5a71233d2ad7afc35183cbc1e8
vic <vpicon98@gmail.com>
2020-11-24 20:09:29 +0100

    Add Output abstract class empty virtual destructor

---
+++ src/player/Output.cpp
@@ -0,0 +1,27 @@
+/**
+ * Output.cpp
+ *
+ * Module: player
+ *
+ * Implements Output abstract class.
+ *
+ */
+
+#include "Output.h"
+
+
+namespace player {
+
+
+/**
+ * Empty destructor in the base class Output, for when the destructor
+ * is called from other derived classes' destructor.
+ */
+Output::~Output() 
+{ 
+}
+
+
+
+}  // namespace player
+
