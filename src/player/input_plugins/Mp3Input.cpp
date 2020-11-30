7b9c8577bf7940b28e162a6fa18ca9afe7d5135c
vic <vpicon98@gmail.com>
2020-11-30 08:47:48 +0100

    Implement Mp3Input constructor and destructor

--- src/player/input_plugins/Mp3Input.cpp
+++ src/player/input_plugins/Mp3Input.cpp
@@ -12,9 +12,6 @@
 #include "Buffer.h"
 
 #include <string>
-#include <fstream>
-#include <iostream>
-#include <cstdint>
 
 
 namespace player {
@@ -22,17 +19,60 @@ namespace player {
 
 // TODO: change order buffer filename
 // TODO: define specific error types
-// TODO: is a stub
 Mp3Input::Mp3Input(const std::string& filename)
     : _filename{filename}
 {
+    int rc;
+    bool isInit, isHandle, isOpen;
+    
+    isInit = true;
+    // Initiate mpg123 library
+    if ((rc = mpg123_init()) != MPG123_OK) {
+        cleanup(isInit);
+        std::runtime_error("Could not initiate mpg123: " + 
+                           std::string{ mpg123_plain_strerror(rc) });
+    }
+    
+    isHandle = true;
+    // Create an mpg123 obscure handle
+    _handle = mpg123_new(nullptr /* default decoder */, &rc);
+    if (_handle == nullptr) {
+        cleanup(isInit, isHandle);
+        std::runtime_error("Could not create mpg123 handle: " + 
+                           std::string{ mpg123_plain_strerror(rc) });
+    }
+
+    isOpen = true;
+    // Open the file with mpg123
+    if ((rc = mpg123_open(_handle, _filename.c_str())) != MPG123_OK) {
+        cleanup(isInit, isHandle, isOpen);
+        std::runtime_error("Could not open file: " + 
+                           std::string{ mpg123_plain_strerror(rc) });
+    }
+
+    // Get sample format of the audio data
+    long rate;
+    int channels, encoding;
+    if ((rc = mpg123_getformat(_handle, &rate, &channels, &encoding)) != MPG123_OK) {
+        cleanup(isInit, isHandle, isOpen);
+        std::runtime_error("Could not get format from input: " + 
+                           std::string{ mpg123_plain_strerror(rc) });
+    }
+
+    _sampleFormat = SampleFormat{static_cast<int>(rate),
+                                 channels,
+                                 0, 
+                                 SampleFormat::Endian::little,
+                                 SampleFormat::Encoding::signedEnc};
 }
 
 
 
 // TODO: is a stub
-Mp3Input::~Mp3Input()
+Mp3Input::~Mp3Input() 
 {
+    bool init{true}, handle{true}, open{true};
+    cleanup(init, handle, open);
 }
 
 
@@ -61,5 +101,27 @@ bool Mp3Input::reachedEOF() const {
 double Mp3Input::getDuration() const {
     return 0.0;
 }
+
+
+
+/**
+ * Define this C-style cleanup routine to ensure that cleanup is done 
+ * in the right order if we exit constructor by failure. For example,
+ * we free the handle before we exit the mpg123 library.
+ */
+void Mp3Input::cleanup(bool init, bool handle, bool open) {
+    // Close opened files by mpg123
+    if (open)
+        mpg123_close(_handle);
+    // Free resources allocated by handle
+    if (handle)
+        mpg123_delete(_handle);
+    // Exit mpg123 library
+    if (init)
+        mpg123_exit();
+}
+
+
+
 
 }  // namespace player
