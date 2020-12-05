018d2a7ab62ec6337c677fcd821185291a758109
vic <vpicon98@gmail.com>
2020-12-05 23:08:37 +0100

    Add database test makefile

--- test/database/Makefile.mk
+++ test/database/Makefile.mk
@@ -1,6 +1,30 @@
-all:
-	echo "test/database makefile"
+module := database
 
-.PHONY:
-clean:
-	echo "stub cleaning"
+# Compilation options and flags
+include_dirs := ../../$(INCLUDE_DIR)/$(module)
+include_dirs += ../../$(LIB_DIR)/gtest/googletest/include
+
+
+CXXFLAGS += $(addprefix -I,$(include_dirs))
+
+
+# Tracked Files
+sources := $(wildcard *.cpp)
+
+headers := $(wildcard ../../$(INCLUDE_DIR)/$(module)/*.h)
+
+test_objects := $(patsubst %.cpp,../../$(BIN_DIR)/test/$(module)/%.o,$(sources))
+
+
+# Targets
+.PHONY: all
+all: $(test_objects)
+
+# Compilation rules
+../../$(BIN_DIR)/test/$(module)/%.o: %.cpp $(headers)
+	$(CXX) $(CXXFLAGS) $< -o $@ -c 
+
+
+.PHONY: clean
+clean: 
+	-rm $(test_objects)
