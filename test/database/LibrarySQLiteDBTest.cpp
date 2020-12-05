00f2dc8b4c7ac346f8dd2f77bc08b04c01750da5
vic <vpicon98@gmail.com>
2020-12-05 23:08:58 +0100

    Add LibrarySQLiteDB initial empty unit tests

---
+++ test/database/LibrarySQLiteDBTest.cpp
@@ -0,0 +1,41 @@
+/** 
+ * LibrarySQLiteDBTest.cpp
+ *
+ * Module: test/database
+ *
+ * Unit tests of the LibrarySQLiteDBTest class.
+ *
+ */
+
+#include "gtest/gtest.h"
+#include "LibrarySQLiteDB.h"
+
+
+
+namespace {
+
+
+// HELPER METHODS
+
+
+// FIXTURE
+
+class LibrarySQLiteDBTest : public testing::Test {
+protected:
+    // No SetUp() needed
+    // No TearDown() needed
+};
+
+// UNIT TESTS
+
+/**
+ * Trivial empty test
+ */
+
+TEST_F(LibrarySQLiteDBTest, Assertion) {
+    ASSERT_TRUE(false);
+}
+
+
+
+}  // namespace 
