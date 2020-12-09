ab10acc84776d10367d7e8660114ca59e3d01d7d
vic <vpicon98@gmail.com>
2020-12-09 20:32:06 +0100

    Add main layout to the main window

--- include/ui/MainWindow.h
+++ include/ui/MainWindow.h
@@ -10,24 +10,40 @@
 #ifndef _VPLAYER_MAIN_WINDOW_H
 #define _VPLAYER_MAIN_WINDOW_H
 
-#include <gtkmm/window.h>
+
+// #include "PlayerWidget.h"
+
+#include <gtkmm/applicationwindow.h>
+#include <gtkmm/box.h>
+#include <gtkmm/paned.h>
+#include <gtkmm/button.h>
 
 
 namespace ui {
 
 
 /**
- * Main window widget of the application. 
+ * Toplevel window widget of the application. 
  */
-class MainWindow : public Gtk::Window {
+class MainWindow : public Gtk::ApplicationWindow {
 public:
     MainWindow();
     ~MainWindow();
+
+private:
+    Gtk::Box _playerLibraryBox;  // two subcontainers separating player from upview
+    Gtk::Paned _libraryViewPane; // Side by side panes of library and view widget
+
+    // PlayerWidget _playerWidget;
+    Gtk::Button _player;         // temporary button object to simulate the player widget
+    Gtk::Button b1{"Hola"};
+    Gtk::Button b2{"Adeu"};
 };
 
 
 
 }  // namespace ui 
+
 
 
 #endif  // _VPLAYER_MAIN_WINDOW_H
--- src/ui/MainWindow.cpp
+++ src/ui/MainWindow.cpp
@@ -14,11 +14,33 @@ namespace ui {
 
 
 MainWindow::MainWindow()
+    : _playerLibraryBox {Gtk::Orientation::ORIENTATION_VERTICAL},
+      _libraryViewPane  {Gtk::Orientation::ORIENTATION_HORIZONTAL},
+      _player {"Player"}
 {
+    // Set window properties
     set_title("VPlayer Music");
     maximize();
-}
 
+    // Set the horizontal pane in the window
+    add(_playerLibraryBox);
+
+    // Add elements to the box and set spacing
+    _playerLibraryBox.pack_start(_libraryViewPane, 
+                                 Gtk::PackOptions::PACK_EXPAND_WIDGET);
+    _playerLibraryBox.pack_end(_player, 
+                               Gtk::PackOptions::PACK_SHRINK);
+
+    int boxInnerSpacing = 1;
+    _playerLibraryBox.set_spacing(boxInnerSpacing);
+
+    // Add elements to the pane
+    _libraryViewPane.add1(b1);
+    _libraryViewPane.add2(b2);
+
+    // Show widgets
+    show_all_children();
+}
 
 
 
