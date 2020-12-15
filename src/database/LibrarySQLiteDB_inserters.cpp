9ae009edb612f78d7c3a701192f4b284c4f39a1d
vic <vpicon98@gmail.com>
2020-12-15 11:39:09 +0100

    Add stubs to LibrarySQLiteDB inserter methods

--- src/database/LibrarySQLiteDB_inserters.cpp
+++ src/database/LibrarySQLiteDB_inserters.cpp
@@ -13,6 +13,38 @@
 namespace database {
 
 
+// TODO: is a stub
+void LibrarySQLiteDB::insertNewTrack() {
+    return;
+}
+
+
+
+// TODO: is a stub
+void LibrarySQLiteDB::insertNewPlaylist() {
+    return;
+}
+
+
+
+// TODO: is a stub
+void LibrarySQLiteDB::insertTrackToPlaylist() {
+    return;
+}
+
+
+
+// TODO: is a stub
+void LibrarySQLiteDB::insertNewAlbum() {
+    return;
+}
+
+
+
+// TODO: is a stub
+void LibrarySQLiteDB::insertNewArtist() {
+    return;
+}
 
 
 
