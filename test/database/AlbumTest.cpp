a5cda89506d99d8dd317245d8a18a6a1317fafb5
vic <vpicon98@gmail.com>
2020-12-16 09:25:50 +0100

    Add Album tests for artist getter and setters

--- test/database/AlbumTest.cpp
+++ test/database/AlbumTest.cpp
@@ -9,6 +9,7 @@
 
 #include "gtest/gtest.h"
 #include "Album.h"
+#include "Artist.h"
 
 #include <string>
 
@@ -17,6 +18,13 @@ namespace {
 
 
 // HELPER METHODS
+
+bool artistsEqual(const database::Artist &artist1, const database::Artist &artist2) {
+    EXPECT_EQ(artist1.getId(), artist2.getId());
+    EXPECT_EQ(artist1.getName(), artist2.getName());
+    EXPECT_EQ(artist1.getImgSource(), artist2.getImgSource());
+    EXPECT_EQ(artist1.getBio(), artist2.getBio());
+}
 
 
 // FIXTURE
@@ -29,10 +37,11 @@ protected:
     // Generate a new artist
     int id {10};
     std::string title {"MiniAlbum"};
+    database::Artist artist {"John Butler", "johnny.jpeg", "A nice guitar player."};
     int year {1970};
     std::string imgSource {"/home/usr/some_image.jpg"};
 
-    database::Album album {id, title, year, imgSource};
+    database::Album album {id, title, artist, year, imgSource};
 
     // Generate a default constructor empty artist
     database::Album emptyAlbum {};
@@ -47,6 +56,7 @@ protected:
 TEST_F(AlbumTest, getters) {
     EXPECT_EQ(album.getId(), id);
     EXPECT_EQ(album.getTitle(), title);
+    artistsEqual(album.getArtist(), aritst);
     EXPECT_EQ(album.getYear(), year);
     EXPECT_EQ(album.getImgSource(), imgSource);
 }
@@ -58,6 +68,7 @@ TEST_F(AlbumTest, getters) {
 TEST_F(AlbumTest, defaultConstructor) {
     EXPECT_EQ(emptyAlbum.getId(), 0);
     EXPECT_EQ(emptyAlbum.getTitle(), "");
+    artistsEqual(emptyArtist.getArtist(), Artist{});
     EXPECT_EQ(emptyAlbum.getYear(), 0);
     EXPECT_EQ(emptyAlbum.getImgSource(), "");
 }
@@ -69,18 +80,23 @@ TEST_F(AlbumTest, defaultConstructor) {
 
 TEST_F(AlbumTest, setters) {
     // Define new values
+    int newId {81241};
     std::string newTitle {"OtherAlbum"};
+    database::Artist newArtist {"Other name", "joeg", "Ar."};
     int newYear {2000};
     std::string newImgSource {"/home/usr/x.jpg"};
 
     // Set new values
+    album.setId(newId);
     album.setTitle(newTitle);
+    album.setArtist(newArtist);
     album.setYear(newYear);
     album.setImgSource(newImgSource);
 
     // Test new values
     EXPECT_EQ(album.getId(), id);
     EXPECT_EQ(album.getTitle(), newTitle);
+    artistsEqual(album.getArtist(), newArtist);
     EXPECT_EQ(album.getYear(), newYear);
     EXPECT_EQ(album.getImgSource(), newImgSource);
 }
