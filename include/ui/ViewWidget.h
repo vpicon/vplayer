26b99ecb2a9cb30b4bcd387c1589b5736920b3be
vic <vpicon98@gmail.com>
2021-03-21 21:16:43 +0100

    Implement simple ViewWidget class structure

--- include/ui/ViewWidget.h
+++ include/ui/ViewWidget.h
@@ -10,7 +10,10 @@
 #ifndef _VPLAYER_VIEW_WIDGET_H
 #define _VPLAYER_VIEW_WIDGET_H
 
-#include <gtkmm/button.h>
+#include "SongsViewWidget.h"
+
+#include <gtkmm/box.h>
+#include <gtkmm/label.h>
 
 
 namespace ui {
@@ -21,13 +24,16 @@ namespace ui {
  * the music player, contains one kind of view at a time, depending
  * on what the user wants to see.
  */
-class ViewWidget : public Gtk::Button {
+class ViewWidget : public Gtk::Box {
 public:
     ViewWidget();
     ~ViewWidget();
 private:
+    Gtk::Label _topBar;
+    SongsViewWidget _songsView;
+
     // TODO:
-    // Add a top bar (transparent and minimal), with a stack controller
+    // Add a top bar (transparent and minimal) with and a stack controller
     // (previous and next window in stack), and a search bar.
     //
     // And a main stack widget. This will contain each of the views (or pages)
--- src/ui/ViewWidget.cpp
+++ src/ui/ViewWidget.cpp
@@ -14,8 +14,20 @@ namespace ui {
 
 
 ViewWidget::ViewWidget()
-    : Gtk::Button{"View"} // TODO temporal button
+    : Gtk::Box{Gtk::Orientation::ORIENTATION_VERTICAL},
+      _topBar{"TOP BAR", Gtk::Align::ALIGN_START}
 {
+    // Add widgets to the box
+    pack_start(_topBar, Gtk::PackOptions::PACK_SHRINK);
+    pack_end(_songsView, Gtk::PackOptions::PACK_EXPAND_WIDGET);
+
+    // Add box of playlists to the scrolled window 
+    // _scrollWin.add(_viewport);
+    // _viewport.add(_playlistsBox);  
+    // _playlistsBox.pack_start(_tmpPlaylist, Gtk::PackOptions::PACK_SHRINK);
+
+    // Show widgets
+    show_all_children();
 }
 
 
